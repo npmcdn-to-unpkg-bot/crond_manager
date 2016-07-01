77cf311a7e5325ef4cf31b36a3d97363b10b32c0
yuzy <yuzy@mysoft.com.cn>
2016-07-02 07:22:10 +0800

    teet

--- common/service/CronFileService.php
+++ common/service/CronFileService.php
@@ -70,7 +70,7 @@ class CronFileService
         }
         $dirInfo=\Yii::$app->basePath."/cron_logs/info/".$guid;
         $dirError=\Yii::$app->basePath."/cron_logs/error/".$guid;
-        $cmdTemplate = ' . /etc/profile;%s;1>>%s.log 2>>%s.log';
+        $cmdTemplate = ' . /etc/profile;%s 1>>%s.log 2>>%s.log';
         $command = sprintf($cmdTemplate,$command,$dirInfo,$dirError);
 
 
