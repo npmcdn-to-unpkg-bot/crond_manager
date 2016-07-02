f0c89ae335114cd30b5ccc707a4bbd127bae7c54
yuzy <yuzy@mysoft.com.cn>
2016-07-02 10:21:11 +0800

    test

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'debug'=>true,
+    'debug'=>false,
     'hdmonitor_emailto'=>'yuzy@mysoft.com.cn',
     'hdmonitor_emailfrom'=>'admin@mysoft.com.cn',
     'hdmonitor_cpu_max'=>90,
@@ -15,6 +15,14 @@ return [
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.exmail.qq.com',
+                'username' => 'yunkeadmin@mysoft.com.cn',
+                'password' => 'Myscrm123456',
+                'port' => 465,
+                'encryption' => 'ssl',
+            ],
         ],
     ],
 ];
