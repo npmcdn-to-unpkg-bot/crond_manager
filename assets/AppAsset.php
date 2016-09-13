2b9cb3b21bc152907769cef27c2ab2e01e5205d8
npm-to-cdn-bot (by Forbes Lindesay) <npmcdn-to-unpkg-bot@users.noreply.github.com>
2016-09-13 19:25:27 +0100

    Replace npmcdn.com with unpkg.com

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,21 +21,21 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
-        'https://npmcdn.com/core-js/client/shim.min.js',
-        'https://npmcdn.com/zone.js@0.6.12?main=browser',
-        'https://npmcdn.com/reflect-metadata@0.1.3',
-        //'https://npmcdn.com/rxjs@5.0.0-beta.6/bundles/Rx.umd.js',
-        'https://npmcdn.com/systemjs@0.19.27/dist/system.src.js'
-        /*'https://npmcdn.com/core-js/client/shim.min.js',
-        'https://npmcdn.com/zone.js@0.6.12?main=browser',
-        'https://npmcdn.com/reflect-metadata@0.1.3',
-        'https://npmcdn.com/rxjs@5.0.0-beta.6/bundles/Rx.umd.js',
-        'https://npmcdn.com/@angular/core/core.umd.js',
-        'https://npmcdn.com/@angular/common/common.umd.js',
-        'https://npmcdn.com/@angular/compiler/compiler.umd.js',
-        'https://npmcdn.com/@angular/platform-browser/platform-browser.umd.js',
-        'https://npmcdn.com/@angular/platform-browser-dynamic/platform-browser-dynamic.umd.js',
-        'https://npmcdn.com/systemjs@0.19.27/dist/system.src.js'
+        'https://unpkg.com/core-js/client/shim.min.js',
+        'https://unpkg.com/zone.js@0.6.12?main=browser',
+        'https://unpkg.com/reflect-metadata@0.1.3',
+        //'https://unpkg.com/rxjs@5.0.0-beta.6/bundles/Rx.umd.js',
+        'https://unpkg.com/systemjs@0.19.27/dist/system.src.js'
+        /*'https://unpkg.com/core-js/client/shim.min.js',
+        'https://unpkg.com/zone.js@0.6.12?main=browser',
+        'https://unpkg.com/reflect-metadata@0.1.3',
+        'https://unpkg.com/rxjs@5.0.0-beta.6/bundles/Rx.umd.js',
+        'https://unpkg.com/@angular/core/core.umd.js',
+        'https://unpkg.com/@angular/common/common.umd.js',
+        'https://unpkg.com/@angular/compiler/compiler.umd.js',
+        'https://unpkg.com/@angular/platform-browser/platform-browser.umd.js',
+        'https://unpkg.com/@angular/platform-browser-dynamic/platform-browser-dynamic.umd.js',
+        'https://unpkg.com/systemjs@0.19.27/dist/system.src.js'
         */
     ];
     public $depends = [
